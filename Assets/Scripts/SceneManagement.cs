708353468d15ccadb6773808b3ac9746405a4e2c
Kirayshen <kirayshen@gmail.com>
2018-02-22 22:08:47 +0300

    LevelButtonClass

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -16,25 +16,7 @@ public class SceneManagement : MonoBehaviour
 
         if (hitInfo)
         {
-            Debug.Log("Push");
-            btnTag = hitInfo.transform.tag;
-
-            if (btnTag == SceneNames.Start.ToString())                sceneForLoad = SceneNames.Start.ToString();
-            else if (btnTag == SceneNames.SelectLevel.ToString())     sceneForLoad = SceneNames.SelectLevel.ToString();
-            else if (btnTag == SceneNames.SelectCharacter.ToString()) sceneForLoad = SceneNames.SelectCharacter.ToString();
-            else if (btnTag == SceneNames.Battle.ToString())          sceneForLoad = SceneNames.Battle.ToString();
-            else if (btnTag == SceneNames.Level1.ToString())          sceneForLoad = SceneNames.Level1.ToString();
-            else if (btnTag == SceneNames.Level2.ToString())          sceneForLoad = SceneNames.Level2.ToString();
-            else if (btnTag == SceneNames.Level3.ToString())          sceneForLoad = SceneNames.Level3.ToString();
-
-            try
-            {
-                SceneManager.LoadScene(sceneForLoad);
-            }
-            catch (System.Exception)
-            {
-                Debug.Log(string.Format("You are trying to load incorrect scene {0}", sceneForLoad));
-            }
+           
             
         }
         else
