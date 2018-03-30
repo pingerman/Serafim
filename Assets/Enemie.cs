6cc42d141d402eb872998e254fd91bf9a0ba4750
Kirayshen <kirayshen@gmail.com>
2018-03-30 10:42:19 +0300

    Enemie and batlle panel

---
+++ Assets/Enemie.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Класс, определяющий вражескую единицу
+/// </summary>
+public class Enemie : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        //TODO вызвать панель боя    
+    }
+}
